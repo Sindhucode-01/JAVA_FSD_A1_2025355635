122160703f151426aa2a7f9880148a350208ee49
V. Divya Sindhu <sindhu.vd8607@gmail.com>
2026-08-06 23:29:42 +0530

    Hello Java Program

---
+++ P01_HelloJava.java
@@ -0,0 +1,8 @@
+public class P01_HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Name   : Varada Divya Sindhu");
+        System.out.println("Roll   : 2025355635");
+        System.out.println("Branch : CSE (2nd Year)");
+        System.out.println("Quote  : \"Think. Code. Debug. Repeat\"");
+    }
+}
